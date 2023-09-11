01191fe20b925b7c2daaeba4e4d0b5917ddf5bb4
AnjaliPPrajapati <134782477+AnjaliPPrajapati@users.noreply.github.com>
2023-09-11 18:29:29 +0530

    Update views.py

--- crud_application/views.py
+++ crud_application/views.py
@@ -5,6 +5,7 @@ import time
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from crud_application.models import *
+from django.urls import reverse
 
 @csrf_exempt
 def add_student(request):
@@ -17,11 +18,25 @@ def add_student(request):
             student_obj.student_first_name = request_object["first_name"]
             student_obj.student_last_name = request_object["last_name"]
             student_obj.save()
-            response = JsonResponse({"message":"Record inserted successfully"}, status = 200)
+            response = JsonResponse({"message":"Record inserted successfully"})
         else:
-            response = JsonResponse({"message":"Invalid Request Object"}, status = 400)
+            response = JsonResponse({"message":"Invalid Request Object"})
 
     except Exception as error:
         print("Error in add_student: ",error)
-        response = JsonResponse({"message":"sonething went wrong"}, status = 500)
+        response = JsonResponse({"message":"sonething went wrong"})
     return response
+
+def common_test_func(self,req_body,url,message,none_check = None):
+    self.response =self.client.post(
+            reverse(url),
+            req_body,
+            format="json"
+        )
+    response_content = json.loads(self.response.content.decode('utf-8'))
+    self.assertEqual(self.response.status_code,200)
+    if 'message' in response_content:
+        self.assertEqual(message,response_content["message"])
+    if none_check:
+        self.assertIsNotNone(response_content[none_check])
+
